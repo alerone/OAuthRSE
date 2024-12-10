f89bd0b42a0b98a5c3c8b5f22e073e5086f4cd4d
Álvaro López <lopezalvarezalvaro1@gmail.com>
2024-12-10 13:33:43 +0100

    feat: delete todo route added

--- db/todo_crud.py
+++ db/todo_crud.py
@@ -17,6 +17,25 @@ def create_todo(name: str, description: str, created_by: int):
     
     finally:
         db.close()
+
+def delete_todo(todo: ToDo):
+    with SessionLocal() as db:
+        try:
+            db.delete(todo)
+            db.commit()
+            return True
+        except Exception as e:
+            db.rollback()
+            print("Error al borrar la To Do:", e)
+            return False
+        
+def get_todo_by_id(todo_id: int):
+    with SessionLocal() as db:
+        try:
+            return db.query(ToDo).filter(ToDo.id == todo_id).options(joinedload(ToDo.creator)).first()
+        except Exception as e:
+            print("Error al buscar la To Do:", e)
+
     
 def get_all_todos():
     with SessionLocal() as db:
--- routes/todos.py
+++ routes/todos.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-from db.todo_crud import create_todo
+from db.todo_crud import create_todo, get_todo_by_id, delete_todo
 
 todo_bp = Blueprint('todo', __name__)
 
@@ -23,3 +23,24 @@ def create():
         return redirect(url_for('todo.create'))
     
     return render_template('create_todo.html')
+
+@todo_bp.route('/delete/<int:todo_id>', methods=['POST'])
+def delete(todo_id: int):
+    if 'user' not in session:
+        flash("Debes iniciar sesión para crear una tarea", 'error')
+        return redirect(url_for('auth.login'))
+    todo = get_todo_by_id(todo_id)
+
+    if not todo:
+        flash('La tarea no existe.', 'error')
+        return redirect(url_for('home'))
+    
+    if todo.created_by != session['user']['id']:
+        flash('No tienes permiso para borrar esta tarea.', 'error')
+        return redirect(url_for('home'))
+    if delete_todo(todo):
+        flash('Tarea eliminada con éxito.', 'success')
+    else:
+        flash('Error al intentar eliminar la tarea.', 'error')
+        
+    return redirect(url_for('home'))
